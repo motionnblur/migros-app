15b0593e98a7cddb737a9972e5b72b098fffc6f9
motionnblur <carbonfiberdev@gmail.com>
2024-12-18 14:27:01 +0300

    Add discover area

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DiscoverComponent } from '../../components/discover-area/parent/discover-area.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DiscoverComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'migros-app';
