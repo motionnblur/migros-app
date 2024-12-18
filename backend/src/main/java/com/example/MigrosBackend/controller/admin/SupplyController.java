c90566abfb34ed0435c5571c70a2f1f18594256e
motionnblur <carbonfiberdev@gmail.com>
2024-12-19 00:17:12 +0300

    Add supply service
    + some refactor

--- backend/src/main/java/com/example/MigrosBackend/controller/admin/SupplyController.java
+++ backend/src/main/java/com/example/MigrosBackend/controller/admin/SupplyController.java
@@ -5,6 +5,7 @@ import com.example.MigrosBackend.entity.CategoryEntity;
 import com.example.MigrosBackend.entity.ItemEntity;
 import com.example.MigrosBackend.repository.CategoryEntityRepository;
 import com.example.MigrosBackend.repository.ItemEntityRepository;
+import com.example.MigrosBackend.service.SupplyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,32 +15,24 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("admin/supply")
 public class SupplyController {
     @Autowired
-    private CategoryEntityRepository categoryEntityRepository;
-    @Autowired
-    private ItemEntityRepository itemEntityRepository;
+    private SupplyService supplyService;
 
     @GetMapping("addCategory")
-    private ResponseEntity<?> addCategory(@RequestParam String categoryName) {
-        CategoryEntity categoryEntity = new CategoryEntity();
-        categoryEntity.setCategoryName(categoryName);
-
-        categoryEntityRepository.save(categoryEntity);
-
-        return new ResponseEntity<>(HttpStatus.OK);
+    private ResponseEntity<?> addCategory(@RequestParam String categoryName) throws Exception {
+        try{
+            supplyService.addCategory(categoryName);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
     @PostMapping("addItem")
-    private ResponseEntity<?> addItem(@RequestBody ItemDto itemDto) {
-        CategoryEntity categoryEntity = categoryEntityRepository.findByCategoryName(itemDto.getCategoryName());
-
-        ItemEntity itemEntity = new ItemEntity();
-        itemEntity.setItemName(itemDto.getItemName());
-        itemEntity.setItemCount(itemDto.getItemCount());
-        itemEntity.setItemPrice(itemDto.getItemPrice());
-        itemEntity.setDiscount(itemDto.getDiscount());
-        itemEntity.setCategoryEntity(categoryEntity);
-
-        itemEntityRepository.save(itemEntity);
-
-        return new ResponseEntity<>(HttpStatus.OK);
+    private ResponseEntity<?> addItem(@RequestBody ItemDto itemDto) throws Exception {
+        try{
+            supplyService.addItem(itemDto);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 }
---
+++ backend/src/main/java/com/example/MigrosBackend/service/SupplyService.java
@@ -0,0 +1,37 @@
+package com.example.MigrosBackend.service;
+
+import com.example.MigrosBackend.dto.ItemDto;
+import com.example.MigrosBackend.entity.CategoryEntity;
+import com.example.MigrosBackend.entity.ItemEntity;
+import com.example.MigrosBackend.repository.CategoryEntityRepository;
+import com.example.MigrosBackend.repository.ItemEntityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SupplyService {
+    @Autowired
+    private CategoryEntityRepository categoryEntityRepository;
+    @Autowired
+    private ItemEntityRepository itemEntityRepository;
+
+    public void addCategory(String categoryName) throws Exception {
+        CategoryEntity categoryEntity = new CategoryEntity();
+        categoryEntity.setCategoryName(categoryName);
+
+        categoryEntityRepository.save(categoryEntity);
+    }
+    public void addItem(ItemDto itemDto) throws Exception {
+        CategoryEntity categoryEntity = categoryEntityRepository.findByCategoryName(itemDto.getCategoryName());
+        if(categoryEntity == null) throw new Exception("Category: " +itemDto.getCategoryName()+ " could not be found");
+
+        ItemEntity itemEntity = new ItemEntity();
+        itemEntity.setItemName(itemDto.getItemName());
+        itemEntity.setItemCount(itemDto.getItemCount());
+        itemEntity.setItemPrice(itemDto.getItemPrice());
+        itemEntity.setDiscount(itemDto.getDiscount());
+        itemEntity.setCategoryEntity(categoryEntity);
+
+        itemEntityRepository.save(itemEntity);
+    }
+}
