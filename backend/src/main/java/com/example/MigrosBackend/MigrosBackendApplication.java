70c736983ab2913a0a6ef368bf224600d8f42ec6
motionnblur <carbonfiberdev@gmail.com>
2024-12-18 17:02:17 +0300

    Add spring boot
    Added spring boot to the backend folder

---
+++ backend/src/main/java/com/example/MigrosBackend/MigrosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.MigrosBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrosBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/MigrosBackend/MigrosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MigrosBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
