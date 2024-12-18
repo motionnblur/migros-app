0665b49e1464db89f845d456ddf484d73fbab1af
motionnblur <carbonfiberdev@gmail.com>
2024-12-18 20:26:02 +0300

    Add category entity

---
+++ backend/src/main/java/com/example/MigrosBackend/entity/CategoryEntity.java
@@ -0,0 +1,24 @@
+package com.example.MigrosBackend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryEntity {
+    @Id
+    @Column(name = "category_entity_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String categoryName;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "category_entity_id", referencedColumnName = "todo_entity_id")
+    private List<ItemEntity> itemEntities;
+}
