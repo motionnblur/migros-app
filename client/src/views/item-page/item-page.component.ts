4fc1d7a5c860b9bb375d00dbd6e250a3f845029e
motionnblur <carbonfiberdev@gmail.com>
2024-12-19 21:56:35 +0300

    # Add item page to client

---
+++ client/src/views/item-page/item-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-page',
+  imports: [],
+  templateUrl: './item-page.component.html',
+  styleUrl: './item-page.component.css'
+})
+export class ItemPageComponent {
+
+}
