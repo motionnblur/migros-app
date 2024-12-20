65ec7c15366df28e1712503ef953729735211967
motionnblur <carbonfiberdev@gmail.com>
2024-12-20 19:54:37 +0300

    # Working on item preview - 2

--- client/src/views/item-page/item-page.component.ts
+++ client/src/views/item-page/item-page.component.ts
@@ -1,16 +1,30 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { RestService } from '../../services/rest/rest.service';
 import { ItemPreviewComponent } from '../item-preview/item-preview.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-item-page',
-  imports: [ItemPreviewComponent],
+  imports: [ItemPreviewComponent, NgFor],
   templateUrl: './item-page.component.html',
   styleUrl: './item-page.component.css',
 })
 export class ItemPageComponent implements OnInit {
-  constructor(public restService: RestService) {}
   @Input() itemId!: number;
+  items = [
+    { title: 'item1', price: 100 },
+    { title: 'item2', price: 200 },
+    { title: 'item3', price: 300 },
+    { title: 'item4', price: 400 },
+    { title: 'item5', price: 500 },
+    { title: 'item6', price: 600 },
+    { title: 'item7', price: 700 },
+    { title: 'item8', price: 800 },
+    { title: 'item9', price: 900 },
+    { title: 'item10', price: 1000 },
+  ];
+
+  constructor(public restService: RestService) {}
 
   ngOnInit(): void {
     this.restService.getItemPageData(1, 0, 10).subscribe((data) => {
