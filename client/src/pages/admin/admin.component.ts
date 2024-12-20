0e8523fecf19da46ab4a8385449729dea2289e53
motionnblur <carbonfiberdev@gmail.com>
2024-12-21 01:39:15 +0300

    Add sign up component

--- client/src/pages/admin/admin.component.ts
+++ client/src/pages/admin/admin.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 import { AdminLoginComponent } from './components/admin-login/admin-login.component';
+import { CommonModule } from '@angular/common';
+import { AdminSignupComponent } from './components/admin-signup/admin-signup.component';
 
 @Component({
   selector: 'app-admin',
-  imports: [AdminLoginComponent],
+  imports: [
+    AdminLoginComponent,
+    AdminSignupComponent,
+    CommonModule,
+    AdminSignupComponent,
+  ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css',
 })
-export class AdminComponent {}
+export class AdminComponent {
+  isOnLogin = false;
+  hasLoginPhaseStarted() {
+    return this.isOnLogin;
+  }
+}
---
+++ client/src/pages/admin/components/admin-signup/admin-signup.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-signup',
+  imports: [],
+  templateUrl: './admin-signup.component.html',
+  styleUrl: './admin-signup.component.css'
+})
+export class AdminSignupComponent {
+
+}
