28d3e55d01382367b2e6f5e4082e37db040e019f
motionnblur <carbonfiberdev@gmail.com>
2024-12-21 22:08:53 +0300

    Save user password encrypted

--- backend/src/main/java/com/example/MigrosBackend/service/admin/sign/AdminSignupService.java
+++ backend/src/main/java/com/example/MigrosBackend/service/admin/sign/AdminSignupService.java
@@ -1,5 +1,6 @@
 package com.example.MigrosBackend.service.admin.sign;
 
+import com.example.MigrosBackend.config.SecurityConfiguration;
 import com.example.MigrosBackend.dto.admin.sign.AdminSignDto;
 import com.example.MigrosBackend.entity.AdminEntity;
 import com.example.MigrosBackend.repository.AdminEntityRepository;
@@ -9,16 +10,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class AdminSignupService {
     private final AdminEntityRepository adminEntityRepository;
+    private final SecurityConfiguration securityConfiguration;
 
     @Autowired
-    public AdminSignupService(AdminEntityRepository adminEntityRepository) {
+    public AdminSignupService(AdminEntityRepository adminEntityRepository, SecurityConfiguration securityConfiguration) {
         this.adminEntityRepository = adminEntityRepository;
+        this.securityConfiguration = securityConfiguration;
     }
 
     public void signup(AdminSignDto adminSignDto) {
         AdminEntity adminEntityToCreate = new AdminEntity();
         adminEntityToCreate.setAdminName(adminSignDto.getAdminName());
-        adminEntityToCreate.setAdminPassword(adminSignDto.getAdminPassword());
+        adminEntityToCreate.setAdminPassword(securityConfiguration.passwordEncoder().encode(adminSignDto.getAdminPassword()));
 
         boolean b = adminEntityRepository.existsByAdminName(adminSignDto.getAdminName());
         if(b) throw new RuntimeException("Admin with that name: "+ adminSignDto.getAdminName()+" already exists.");
@@ -31,7 +34,7 @@ public class AdminSignupService {
         if(!b) throw new RuntimeException("Admin with that name: "+ adminSignDto.getAdminName()+" could not be found.");
 
         AdminEntity adminEntity = adminEntityRepository.findByAdminName(adminSignDto.getAdminName());
-        if(!adminEntity.getAdminPassword().equals(adminSignDto.getAdminPassword()))
+        if(!securityConfiguration.passwordEncoder().matches(adminSignDto.getAdminPassword(), adminEntity.getAdminPassword()))
             throw new RuntimeException("Wrong password.");
     }
 }
