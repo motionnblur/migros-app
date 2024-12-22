8ddf2304fdbeb4f30f0f8c2d663aa11e7232b5c2
motionnblur <carbonfiberdev@gmail.com>
2024-12-22 20:53:47 +0300

    #Product body

--- client/src/pages/admin/components/product-adder/product-adder.component.ts
+++ client/src/pages/admin/components/product-adder/product-adder.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-adder',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './product-adder.component.html',
-  styleUrl: './product-adder.component.css'
+  styleUrl: './product-adder.component.css',
 })
 export class ProductAdderComponent {
-
+  @Input() productName!: string;
+  @Input() price!: number;
+  @Input() count!: number;
+  @Input() discount!: number;
+  @Input() description!: string;
 }
