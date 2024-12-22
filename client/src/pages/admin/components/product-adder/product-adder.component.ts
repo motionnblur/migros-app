7a39589b22e02263d5d3dc5df1fbe193a355851d
motionnblur <carbonfiberdev@gmail.com>
2024-12-22 22:56:04 +0300

    #Improve the client

--- client/src/pages/admin/components/product-adder/product-adder.component.ts
+++ client/src/pages/admin/components/product-adder/product-adder.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RestService } from '../../../../services/rest/rest.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-adder',
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './product-adder.component.html',
   styleUrl: './product-adder.component.css',
 })
@@ -16,6 +17,8 @@ export class ProductAdderComponent {
   @Input() description!: string;
 
   imageInputRef: HTMLElement | null = null;
+  imageRef: HTMLImageElement | null = null;
+  addImageRef: HTMLElement | null = null;
   selectedImage: File | null = null;
 
   constructor(private restService: RestService) {}
@@ -27,6 +30,9 @@ export class ProductAdderComponent {
   }
   onImageSelected(event: any) {
     this.selectedImage = event.target.files[0];
+    this.addImageRef!.style.display = 'none';
+    this.imageRef!.style.display = 'block';
+    this.imageRef!.src = URL.createObjectURL(this.selectedImage!);
   }
   uploadImage() {
     this.restService.uploadImage(this.selectedImage!).subscribe();
@@ -34,5 +40,7 @@ export class ProductAdderComponent {
 
   ngOnInit() {
     this.imageInputRef = document.getElementById('imageUploader');
+    this.addImageRef = document.getElementById('add-image');
+    this.imageRef = document.getElementById('image') as HTMLImageElement;
   }
 }
