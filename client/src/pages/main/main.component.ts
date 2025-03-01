7d522618dd692e54af23342cd1f4f7659a1c5d67
motionnblur <carbonfiberdev@gmail.com>
2025-03-01 17:42:12 +0300

    #Add login component

--- client/src/pages/main/main.component.ts
+++ client/src/pages/main/main.component.ts
@@ -4,15 +4,31 @@ import { ProductPageComponent } from './components/product-page/product-page.com
 import { CommonModule } from '@angular/common';
 import { DiscoverComponent } from './components/discover-area/parent/discover-area.component';
 import { data } from '../../memory/global-data';
+import { LoginComponent } from './components/login/login.component';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-main',
-  imports: [DiscoverComponent, ProductPageComponent, CommonModule],
+  imports: [
+    DiscoverComponent,
+    ProductPageComponent,
+    CommonModule,
+    LoginComponent,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+  ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css',
 })
 export class MainComponent {
   isItemPageOpened: boolean = false;
+  isLoginButtonClicked: boolean = false;
+  isSignPhaseActive: boolean = true;
   title: string = 'migros-app';
 
   constructor(private eventManager: EventService) {
@@ -22,10 +38,19 @@ export class MainComponent {
     });
   }
 
-  hasItemPageOpened(): boolean {
+  public openLoginComponent() {
+    this.isLoginButtonClicked = true;
+  }
+  public hasItemPageOpened(): boolean {
     return this.isItemPageOpened;
   }
-  setItemPageOpened(value: boolean) {
+  public openSign() {
+    this.isSignPhaseActive = false;
+  }
+  public openLogin() {
+    this.isSignPhaseActive = true;
+  }
+  private setItemPageOpened(value: boolean) {
     this.isItemPageOpened = value;
   }
 }
