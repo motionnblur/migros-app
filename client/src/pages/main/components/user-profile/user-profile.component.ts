bb725755bdfd3b55542bfb25e15063c4049d342e
motionnblur <carbonfiberdev@gmail.com>
2025-03-26 20:07:53 +0300

    -- Improve user profile component

--- client/src/pages/main/components/user-profile/user-profile.component.ts
+++ client/src/pages/main/components/user-profile/user-profile.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user-profile',
   imports: [],
   templateUrl: './user-profile.component.html',
-  styleUrl: './user-profile.component.css'
+  styleUrl: './user-profile.component.css',
 })
 export class UserProfileComponent {
-
+  @Output() closeComponentEvent = new EventEmitter<void>();
+  public closeProfileComponent() {
+    this.closeComponentEvent.emit();
+  }
 }
