8bad05ab6893257a6ebb23c8f982113d79689e78
motionnblur <carbonfiberdev@gmail.com>
2025-03-28 20:55:41 +0300

    Improve action panel

--- client/src/pages/admin/components/action-panel/action-panel.component.ts
+++ client/src/pages/admin/components/action-panel/action-panel.component.ts
@@ -8,16 +8,38 @@ import {
 } from '@angular/core';
 import { RestService } from '../../../../services/rest/rest.service';
 import { IUserProfileTable } from '../../../../interfaces/IUserProfileTable';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+
+interface Status {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-action-panel',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+  ],
   templateUrl: './action-panel.component.html',
   styleUrl: './action-panel.component.css',
 })
 export class ActionPanelComponent {
   @Input('orderId') orderId!: number;
   @Output() closeActionPanelEvent = new EventEmitter<void>();
+  public userData!: IUserProfileTable;
+  public status: Status[] = [
+    { value: 'pending', viewValue: 'Pending' },
+    { value: 'inProgress', viewValue: 'In Progress' },
+    { value: 'completed', viewValue: 'Completed' },
+  ];
 
   constructor(private restService: RestService) {}
   ngOnChanges(changes: SimpleChanges) {
@@ -25,7 +47,7 @@ export class ActionPanelComponent {
       this.restService
         .getUserProfileData(this.orderId)
         .subscribe((data: IUserProfileTable) => {
-          console.log(data);
+          this.userData = data;
         });
     }
   }
