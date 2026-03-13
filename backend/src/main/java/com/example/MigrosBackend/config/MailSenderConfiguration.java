9f6ded0e499406bcf47fb6fa63d6e0f73463167d
motionnblur <carbonfiberdev@gmail.com>
2026-03-13 18:29:16 +0300

    fix(security): hide credentials

--- backend/src/main/java/com/example/MigrosBackend/config/MailSenderConfiguration.java
+++ backend/src/main/java/com/example/MigrosBackend/config/MailSenderConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.MigrosBackend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -9,14 +10,20 @@ import java.util.Properties;
 
 @Configuration
 public class MailSenderConfiguration {
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
 
-        mailSender.setUsername("carbonfiberdev@gmail.com");
-        mailSender.setPassword("heig fiqc gtqw bhan");
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
